b18ba1885244f3eb69e29d65248c04310bdb8b3c
Shrashti Jain <jain.shrashti27@gmail.com>
2025-08-30 02:36:40 +0530

    Fix ALLOWED_HOSTS and settings for Render

--- my-project/myproject/settings.py
+++ my-project/myproject/settings.py
@@ -31,7 +31,11 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
+ALLOWED_HOSTS = [
+    "https://women-safety-webapp-vmhz.onrender.com",
+    "localhost",
+    "127.0.0.1"
+]
 
 
 
